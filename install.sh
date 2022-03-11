9d294a705b33a64e1728b34de3bd322622a6d91a
Benjamin Roberts <benjaminrobertsplus1@gmail.com>
2022-03-11 14:40:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+echo "WARNING This script will permenetly OVERRIDE any current nvim config! Are you sure you want to proceed (Y/N)"
+
+read -r answer
+if [[ $answer = "Y" ]] ; then
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-path=$(pwd)
-echo $path
-mv $path ~/.config/nvim
-cd ~/.config/nvim
-nvim init.vim
+	path=$(pwd)
+	echo $path
+	mv $path ~/.config/nvim
+	cd ~/.config/nvim
+	nvim init.vim
+fi
+
+
+
 
 
 # WARNING THIS SCRIPT WILL OVERRIDE YOUR CURRENT NVIM CONFIG
